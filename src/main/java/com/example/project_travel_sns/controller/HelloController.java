86c4dfbf954dfd8c5cbef0035227b1ca4d7e3441
strongcookdas <goodjav141592@gmail.com>
2022-12-24 23:25:26 +0900

    chor : HelloController 수정

--- src/main/java/com/example/project_travel_sns/controller/HelloController.java
+++ src/main/java/com/example/project_travel_sns/controller/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/hello")
-public class helloController {
+public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello(){
-        return ResponseEntity.ok().body("heloo");
+        return ResponseEntity.ok().body("hello");
     }
 }
