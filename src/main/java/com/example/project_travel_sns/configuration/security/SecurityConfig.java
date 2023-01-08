c72083b163d3f3a9a517e60582522c475a29c4c9
strongcookdas <goodjav141592@gmail.com>
2023-01-08 18:39:26 +0900

    refact : 시큐리티 수정

--- src/main/java/com/example/project_travel_sns/configuration/security/SecurityConfig.java
+++ src/main/java/com/example/project_travel_sns/configuration/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.project_travel_sns.configuration.security;
 
-import com.example.project_travel_sns.configuration.security.exception.AccessDeniedManager;
 import com.example.project_travel_sns.configuration.security.exception.AuthenticationManager;
 import com.example.project_travel_sns.configuration.security.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
@@ -15,30 +14,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final AccessDeniedManager accessDeniedManager;
     private final AuthenticationManager authenticationManager;
     private final JwtFilter jwtFilter;
-    private String[] PERMIT_URL = {
+    private final String[] PERMIT_URL = {
             "/api/v1/hello",
-            "/api/v1/users/**"
-    };
-
-    private String[] NOT_PERMIT_URL = {
-            "/api/v1/posts/my"
-    };
-    private String[] SWAGGER = {
-            /* swagger v2 */
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            /* swagger v3 */
-            "/v3/api-docs/**",
-            "/swagger-ui/**"
+            "/api/v1/users/join",
+            "/api/v1/users/login"
     };
 
     @Bean
@@ -49,10 +30,10 @@ public class SecurityConfig {
                 .cors().and()
                 .authorizeRequests()
                 .antMatchers(PERMIT_URL).permitAll()
-                .antMatchers(SWAGGER).permitAll()
-                .antMatchers(NOT_PERMIT_URL).authenticated()
-                .antMatchers(HttpMethod.GET).permitAll()
-                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/v1/posts/my").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
