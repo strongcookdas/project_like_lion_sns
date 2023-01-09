e6a1fbbfda368d1f0f0aa4f4d04d9738865062fd
strongcookdas <goodjav141592@gmail.com>
2023-01-09 18:25:57 +0900

    refact : 사용하지 않는 예외 제거

--- src/main/java/com/example/project_travel_sns/configuration/security/exception/AccessDeniedManager.java
+++ src/main/java/com/example/project_travel_sns/configuration/security/exception/AccessDeniedManager.java
@@ -10,7 +10,6 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,7 +22,7 @@ public class AccessDeniedManager implements AccessDeniedHandler {
     private ObjectMapper objectMapper;
 
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         log.error(ErrorCode.INVALID_PERMISSION.getMessage());
         response.setStatus(ErrorCode.INVALID_PERMISSION.getHttpStatus().value());
         response.setContentType("application/json");
